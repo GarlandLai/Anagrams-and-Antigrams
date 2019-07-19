3d6266f783e00122afa0d40ccbd5b4742bd4889b
Garland Lai <garlandlai24@gmail.com>
2019-07-19 16:01:20 -0700

    clean up and refractor code

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -3,22 +3,16 @@ require ('pry')
 class AnagramAndAntigrams
   attr_reader(:word1,:word2)
   def initialize(word1, word2)
-    @@word1 = word1.downcase
-    @@word2 = word2.downcase
+    @word1 = word1.downcase.split("").sort.reject{|i| i == (" ")}
+    @word2 = word2.downcase.split("").sort.reject{|i| i == (" ")}
   end
-
   def check_words
-    @@word1 = @@word1.split("").sort
-    @@word2 = @@word2.split("").sort
-    @@word1 = @@word1.reject{|i| i == (" ")}
-    @@word2 = @@word2.reject{|i| i == (" ")}
-
-    if @@word1.any?{|letter| ['a','e','i','o','u','y'].include? letter} && @@word2.any?{|letter| ['a','e','i','o','u','y'].include? letter}
-      if @@word1 == @@word2
+    if @word1.any?{|letter| ['a','e','i','o','u','y'].include? letter} && @word2.any?{|letter| ['a','e','i','o','u','y'].include? letter}
+      if @word1 == @word2
         p "These words are anagrams!"
-      elsif @@word1.any?{|letter| @@word2.include? letter} == false
+      elsif @word1.any?{|letter| @word2.include? letter} == false
         p 'The words are antigrams since no letters match!'
-      elsif @@word1.any?{|letter| @@word2.include? letter} == true
+      elsif @word1.any?{|letter| @word2.include? letter} == true
         p 'The words are not anagrams or antigrams.'
       end
     else
